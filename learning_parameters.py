620306389782adf2dd3641308df35e99e37a863f
LucaMaurici <57830854+LucaMaurici@users.noreply.github.com>
2020-06-25 11:49:17 +0200

    Aggiustamenti parametri

--- learning_parameters.py
+++ learning_parameters.py
@@ -18,14 +18,14 @@ solver_iter = 100
 # DMP parameters
 dmp_num_theta = 10  # number of parameters Default: 10
 dmp_rtime = 20  # number of points to be interpolated
-dmp_stime = 50 #35 #200 #100  # number of simulation's timesteps  Default: 100
+dmp_stime = 40 #35 #200 #100  # number of simulation's timesteps  Default: 100
 dmp_dt = 0.25  # integration time over dmp_rtime
 dmp_sigma = 0.05  # standard deviation of each parameters 0.05
 
 # BBO parameters
 bbo_lmb = 0.005# softmax temperature
 bbo_epochs = 50
-bbo_episodes = 40
+bbo_episodes = 20
 bbo_num_dmps = 7 #Original Version 9
 bbo_sigma_max = 0.2                # Default 0.2
 bbo_sigma_arm_scale = 0.001 #0.001 #0.007 #0.001
@@ -33,7 +33,7 @@ bbo_sigma_joint_scales =  [bbo_sigma_arm_scale,
                            bbo_sigma_arm_scale,
                            bbo_sigma_arm_scale,
                            bbo_sigma_arm_scale,
-                           1,10,10]#0.0001,1,1]#0.002,2,2]#0.2,5,5]           #Original Version 1,1,1,1,1]
+                           1,1,1]#0.0001,1,1]#0.002,2,2]#0.2,5,5]           #Original Version 1,1,1,1,1]
 bbo_sigma = bbo_sigma_max * np.hstack(
     [
         np.ones(dmp_num_theta + 2) * x
@@ -52,9 +52,9 @@ dist_dev_beta = 0.4 #0.6   #0.1                                             #Def
 dist_dev_gamma = 0.012 #0.023 #0.012                                          #Default 0.012
 alpha = 1e7 #0.01  #1e4#1e5#1e5 #6e5 #1e4 #1e4 # floor distance                               #Default: 1e4
 beta = 1 #0.02  #0.01#0.01 #0.1 #1 #1 #10     # finger_distance                              #Default: 1
-gamma = 0.1 #0.02#1#1e2 #1e2 #5e6 #1e8 #1e5 # touch * finger_distance                      #Default: 1e8
+gamma = 0.6 #0.02#1#1e2 #1e2 #5e6 #1e8 #1e5 # touch * finger_distance                      #Default: 1e8
 #max_rew = 25#20#5e1 #5e6 #5e5 #5e9                                              #Default: 5e5
-max_rew = 200 #12
+max_rew = 80 #12
 sigma_moving_average = True
 sigma_moving_average_h = 0.2                                    #Default: 0.2
 
